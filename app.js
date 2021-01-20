62bfba55cb2a3b965e0ad91101f743b93df71206
Fatih Ugur <fatihugur@gmail.com>
2021-01-20 15:06:01 +0100

    startGame function added in methods and start game button added in html

--- app.js
+++ app.js
@@ -49,6 +49,13 @@ const app = Vue.createApp({
     },
   },
   methods: {
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.winner = null;
+      this.currentRound = 0;
+    },
+
     attackMonster() {
       this.currentRound++;
       const attackValue = getRandomValue(5, 12);
