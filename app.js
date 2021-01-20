3eb098cbbf94470783977ffebd2a8049147e9337
Fatih Ugur <fatihugur@gmail.com>
2021-01-20 12:01:22 +0100

    attack button activeted  inline ccs for monsterhealth and playerhealth added

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const app = Vue.createApp({
       const attackValue = getRandomValue(5, 12);
       this.monsterHealth -= attackValue;
       //this.monsterHealth = this.monsterHealth - attackValue;
+      this.attackPlayer();
     },
     attackPlayer() {
       const attackValue = getRandomValue(8, 15);
