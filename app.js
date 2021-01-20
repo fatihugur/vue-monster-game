d4476dd6ac755778ec77efa9a1464ec1c867cf3c
Fatih Ugur <fatihugur@gmail.com>
2021-01-20 11:33:17 +0100

    attackMonster and attackPlayer formuel added

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+    };
+  },
+  methods: {
+    attackMonster() {
+      const attackValue = Math.floor(Math.random() * (12 - 5)) + 5;
+      this.monsterHealth -= attackValue;
+      //this.monsterHealth = this.monsterHealth - attackValue;
+    },
+    attackPlayer() {
+      const attackValue = Math.floor(Math.random() * (15 - 8)) + 8;
+      this.playerHealth -= attackValue;
+      //this.playerHealth = this.playerHealth - attackValue;
+    },
+  },
+});
 
 app.mount("#game");
