bf9fce83e54994a1edbda3a0866b25fee93d3d8f
Fatih Ugur <fatihugur@gmail.com>
2021-01-20 12:53:33 +0100

    healPlayer method updated with currentRound value

--- app.js
+++ app.js
@@ -41,12 +41,14 @@ const app = Vue.createApp({
       this.attackPlayer();
     },
     healPlayer() {
+      this.currentRound++;
       const healValue = getRandomValue(8, 20);
       if (this.playerHealth + healValue > 100) {
         this.playerHealth = 100;
       } else {
         this.playerHealth += healValue;
       }
+      this.attackPlayer();
     },
   },
 });
